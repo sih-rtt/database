f9c7234a96e0f470d7d87a84fa520e7ddb1616da
Anmol Sudhir <anmolsudhir.2001@gmail.com>
2023-11-22 18:24:32 +0530

    S33-25 test: Tested @sih-rtt/dbclient

--- prisma/migrations/20231115135401_schema_v2_8_1/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `polyline` on the `Session` table. The data in that column could be lost. The data in that column will be cast from `Text` to `Unsupported("geometry(LINESTRING, 4326)")`.
-
-*/
--- AlterTable
-ALTER TABLE "Session" ALTER COLUMN "polyline" DROP NOT NULL,
-ALTER COLUMN "polyline" SET DATA TYPE geometry(LINESTRING, 4326);
--- prisma/migrations/20231115183059_schema_v2_8_2/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Route" ADD COLUMN     "fromLocation" geometry(POINT, 4326),
-ADD COLUMN     "toLocation" geometry(POINT, 4326);
